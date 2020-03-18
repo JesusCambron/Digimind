c1edf0590836402bf303675c4bf37cda2ab96694
JesusCambron <manuel.cam98@gmail.com>
2020-03-17 20:56:47 -0700

    first commit

---
+++ app/src/main/java/cambron/jesus/digimind/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,31 @@
+package cambron.jesus.digimind.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import cambron.jesus.digimind.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.navigation_dashboard)
+        dashboardViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
